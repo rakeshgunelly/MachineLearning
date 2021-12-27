b92c3ac9ffc8985c2e1342e47e591c72cc977a23
Rakesh Gunelly <rakeshgunelly@gmail.com>
2021-12-27 16:39:40 +0530

    retriveing data from csv

---
+++ 01DataPreprocessing/data_preprocessing_tools.py
@@ -0,0 +1,13 @@
+# Data Preprocessing Tools
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
